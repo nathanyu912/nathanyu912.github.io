0b6152c426250d698abdfd9c44bd28fcad328795
nathanyu912 <nathan.yu@torontomu.ca>
2024-02-26 23:08:53 -0500

    comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//function asks for user input for name
 function promptForName() {
     var name = prompt("Please enter your name:");
     if (name !== null && name !== "") {
@@ -5,16 +6,19 @@ function promptForName() {
     }
 }
 
+//function enlarges image
 function enlargeImage() {
     var img = document.getElementById("myImage");
     img.style.width = "500px"; // Change the width to enlarge the image
     img.style.height = "500px";
 }
 
+//function highlights menu when hovering
 function highlightNavItem(element) {
     element.style.backgroundColor = "lightgray";
 }
 
+//function stops highlighting menu when not hovering
 function resetNavItem(element) {
     element.style.backgroundColor = "transparent";
 }
