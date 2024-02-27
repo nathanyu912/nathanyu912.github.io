35e348d2b06ff619dabfa839de7ff8b65afce91b
nathanyu912 <nathan.yu@torontomu.ca>
2024-02-26 21:21:39 -0500

    a

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function promptForName() {
     var name = prompt("Please enter your name:");
     if (name !== null && name !== "") {
-        document.getElementById("nameOutput").innerText = "Hello, " + name + "!";
+        document.getElementById("nameOutput").innerText = ", " + name + "!";
     }
 }
 
 function enlargeImage() {
     var img = document.getElementById("myImage");
     img.style.width = "300px"; // Change the width to enlarge the image
+    img.style.height = "300px";
 }
 
 
