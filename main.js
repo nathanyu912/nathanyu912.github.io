58881746e197a1c42d27371a8502aee78508fc64
nathanyu912 <nathan.yu@torontomu.ca>
2024-02-26 22:07:02 -0500

    yes

--- main.js
+++ main.js
@@ -28,3 +28,12 @@ function hideDropdown(element) {
     var dropdownContent = element.getElementsByClassName("dropdown-content")[0];
     dropdownContent.style.display = "none";
 }
+
+window.onmouseout = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content"); for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+    openDropdown.classList.remove('show'); }
+    } }
+    }
